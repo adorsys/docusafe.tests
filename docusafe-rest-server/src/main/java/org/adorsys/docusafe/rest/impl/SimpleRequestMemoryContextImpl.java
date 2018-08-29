3e79654bc5ff93e012a1918eeaa965d95973cc06
pspex <git@electronicpeter.de>
2018-08-29 11:59:03 +0200

    SimpleMemoryContext mit Konstante fuer Scope - Scope noch nicht verstnden

--- docusafe-rest-server/src/main/java/org/adorsys/docusafe/rest/impl/SimpleRequestMemoryContextImpl.java
+++ docusafe-rest-server/src/main/java/org/adorsys/docusafe/rest/impl/SimpleRequestMemoryContextImpl.java
@@ -2,6 +2,9 @@ package org.adorsys.docusafe.rest.impl;
 
 import org.adorsys.cryptoutils.exceptions.BaseException;
 import org.adorsys.docusafe.transactional.RequestMemoryContext;
+import org.adorsys.encobject.service.impl.SimpleStorageMetadataImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
@@ -9,6 +12,8 @@ import org.springframework.web.context.request.RequestContextHolder;
  * Created by peter on 09.07.18 at 14:06.
  */
 public class SimpleRequestMemoryContextImpl implements RequestMemoryContext {
+    private final static Logger LOGGER = LoggerFactory.getLogger(SimpleStorageMetadataImpl.class);
+    private final static int SCOPE = 1;
 
     @Override
     public void put(Object key, Object value) {
@@ -18,7 +23,7 @@ public class SimpleRequestMemoryContextImpl implements RequestMemoryContext {
         }
         if (key instanceof  String) {
             String aKey = (String) key;
-            requestAttributes.setAttribute(aKey, value, 1);
+            requestAttributes.setAttribute(aKey, value, SCOPE);
             return;
         }
         throw new BaseException("key is not of Stringtype but " + key.getClass().getName());
@@ -32,10 +37,8 @@ public class SimpleRequestMemoryContextImpl implements RequestMemoryContext {
         }
         if (key instanceof  String) {
             String aKey = (String) key;
-            return requestAttributes.getAttribute(aKey, 1);
+            return requestAttributes.getAttribute(aKey, SCOPE);
         }
         throw new BaseException("key is not of Stringtype but " + key.getClass().getName());
     }
-
-
 }
