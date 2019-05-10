f71d91d93fc63a7924457dcd3a4db59f81ee6081
pspex <pspex@adorsys.de>
2019-05-10 02:10:49 +0200

    DOC-131 fixed version of docusafe

--- docusafe-rest-server/start.local.server.port.9991.filesystem.sh
+++ docusafe-rest-server/start.local.server.port.9991.filesystem.sh
@@ -1,1 +1,1 @@
-mvn -Dspring.config.location=file:./src/main/resources/port.9991.filesystem.yml spring-boot:run
+mvn -DSC-NO-BUCKETPATH-ENCRYPTION -Dspring.config.location=file:./src/main/resources/port.9991.filesystem.yml spring-boot:run
